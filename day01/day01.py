e6d16332a6e34de40b4a7c1337dd5fda43322af9
Stelios Tsagkarakis <tsagarakis.stelios@gmail.com>
2020-12-02 20:09:59 +0200

    Day01 solution

---
+++ day01/day01.py
@@ -0,0 +1,37 @@
+# implementation for the day01 quiz of "advent of code" 
+ 
+from itertools import combinations
+
+def part1(input):
+    front = 1
+    back = 0
+    while (front < len(input) and back < len(input)):
+        sum = sorted[back] + sorted[front]
+        if (sum == 2020):
+            return (sorted[back]*sorted[front])
+        elif (sum > 2020):
+            back += 1
+        else: # sum < 2020
+            front +=1
+
+
+
+def fsum(val):
+      return sum(val) == 2020
+
+def part2(input):
+    res = list(filter(fsum,list(combinations(input, 3))))
+    res = res[0]
+    result = res[0]*res[1]*res[2]
+    return result
+
+input = []
+with open("input.txt") as f:
+    for line in f:
+        input.append(int(line.strip('\n')))
+    sorted = sorted(input)
+    result1 = part1(sorted)
+    print("part1:", result1)
+    result2 = part2(sorted)
+    print("part2:", result2)
+   
